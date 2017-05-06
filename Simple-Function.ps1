6875218ca1fcb0d1dd7ce9727eede61d77ea3c20
Daniel Pedersen <dp@syspeople.dk>
2017-05-06 10:54:45 +0200

    Added Function Deff.

---
+++ Simple-Function.ps1
@@ -0,0 +1,27 @@
+<#
+.SYNOPSIS
+    Short description
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    Example of how to use this cmdlet
+.EXAMPLE
+    Another example of how to use this cmdlet
+#>
+function verb-noun {
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline=$True)]
+        [string[]]$Input1
+    )
+    
+    begin {
+    }
+    
+    process {
+        
+    }
+    
+    end {
+    }
+}
