ae6c9ab2bbc0dc8e0789e508b1f4147d9122704a
Daniel Pedersen <dp@syspeople.dk>
2017-05-10 19:20:02 +0200

    Added Powershell.json Snippet file

---
+++ Convert-ScriptToTemplate.ps1
@@ -0,0 +1,25 @@
+
+function Add-Dollar {
+param(
+        [Parameter(ValueFromPipeline=$True)]
+        $string
+    )
+    if($string -like "*$*"){
+        $string=$string.Replace('$','$$')
+    }
+return $string
+}
+
+$path="C:\Users\Daniel\Desktop\"
+Remove-Item $path\output.txt
+Get-Content -Path $path\input.txt | ForEach-Object -Process {
+    if (([string]$_).TrimStart() -match '^"' ){
+        $_ | Add-Dollar | Out-File -FilePath $path\output.txt -Append
+    }
+    else{
+    "`""+$_+"`"," | Add-Dollar | Out-File -FilePath $path\output.txt -Append
+    }
+    #("`""+$_+"`",").Replace("$","$$") | Out-File -FilePath C:\Users\Daniel\Desktop\output.txt -Append
+}
+$test='"ej'
+$test -match '^"'
--- Simple-Function.ps1
+++ Simple-Function.ps1
@@ -1,4 +1,35 @@
 <#
+        .NOTES
+        ===========================================================================
+        Created by: CreatedBy
+        Organization: Company
+        Filename: FileName
+        ===========================================================================
+        DESCRIPTION
+        Description
+#>
+
+trap {
+    $line = $_.InvocationInfo.ScriptLineNumber
+    $message = $_.Exception.Message
+
+    $errorDescription = ('Caught error in line {0:00}: {1}' -f $line, $message)
+
+    # Write error to screen
+    Write-Output -InputObject $errorDescription
+
+}
+
+
+#region Settings
+
+
+#endregion Settings
+
+
+#region Functions
+
+<#
 .SYNOPSIS
     Short description
 .DESCRIPTION
@@ -8,20 +39,62 @@
 .EXAMPLE
     Another example of how to use this cmdlet
 #>
-function Get-Root {
+function Write-OutTimeStamped {
     [CmdletBinding()]
     param(
         [Parameter(ValueFromPipeline=$True)]
-        [string[]]$Input1
+        [string[]]$Output
     )
     
     begin {
     }
     
     process {
-        
+        if (Get-Variable -Name stopWatch  -ErrorAction SilentlyContinue) {
+"`n[$('{0:D2}' -f $stopWatch.Elapsed.Minutes):$('{0:D2}' -f $stopWatch.Elapsed.Seconds):$('{0:D2}' -f $stopWatch.Elapsed.Milliseconds)] $Output"
+    }
     }
     
     end {
     }
 }
+
+#endregion Functions
+
+#region Init
+
+$ErrorActionPreference = 'Stop'
+
+$stopWatch = [Diagnostics.Stopwatch]::StartNew()
+
+
+Write-Output -InputObject @"
+$('#' * 80)
+    Script description:
+        <<< SCRIPT DESCRIPTION >>>
+
+    Environment Information:
+        Usercontext: $([System.Environment]::UserDomainName)\$([System.Environment]::UserName)
+        Hostname: $env:COMPUTERNAME
+        Host OS version: $([System.Environment]::OSVersion.Version) ($((Get-WmiObject -Class Win32_OperatingSystem).Caption))
+        PSVersion: $($PSVersionTable.PSVersion)
+$('#' * 80)
+"@
+
+Write-OutTimeStamped "Script started"
+#endregion Init
+
+#region Main
+
+for ($i = 0; $i -lt 1000; $i++) {
+    Write-OutTimeStamped "Hej"
+}
+
+#endregion Main
+
+#region Finalize
+
+Write-OutTimeStamped "Script ran through"
+$stopWatch.Stop()
+
+#endregion Finalize
